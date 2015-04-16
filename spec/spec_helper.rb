823b0acfb18d087acfa55e1f0b67641723e52aa1
Mike Dalessio <mike.dalessio@gmail.com>
2015-04-16 10:52:23 -0400

    Check for existence of `tree` before running suite.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ $LOAD_PATH << File.expand_path('../../spec/helpers', __FILE__)
 require 'buildpack/packager'
 require 'file_system_helpers'
 
+unless system("which tree")
+  raise "Please install the `tree` commandline tool."
+end
+
 RSpec.configure do |config|
   config.include FileSystemHelpers
 end
