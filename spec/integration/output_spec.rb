d1001b1d382b847082d18f89f5e681e4330f25d6
Jacques Chester <jchester@pivotal.io>
2016-08-15 17:04:34 -0400

    Update fixtures to use buildpacks.cloudfoundry.org
    [#127357631]
    
    Signed-off-by: James Wen <jrw2175@columbia.edu>

--- spec/integration/output_spec.rb
+++ spec/integration/output_spec.rb
@@ -31,13 +31,9 @@ describe 'Buildpack packager output' do
 
     it 'outputs the dependencies downloaded, their versions, and download source url' do
       expect(subject).to include(<<-HEREDOC)
-Downloading go version 1.6.1 from: https://storage.googleapis.com/golang/go1.6.1.linux-amd64.tar.gz
-  Using go version 1.6.1 with size 81M
-  go version 1.6.1 matches the manifest provided md5 checksum of e277d18dac8a0b9945d4c53adf6e52ee
-
-Downloading go version 1.6.2 from: https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz
-  Using go version 1.6.2 with size 81M
-  go version 1.6.2 matches the manifest provided md5 checksum of ebfb8b38330c8779b121c43433c4b9be
+Downloading go version 1.6.3 from: https://buildpacks.cloudfoundry.org/concourse-binaries/go/go1.6.3.linux-amd64.tar.gz
+  Using go version 1.6.3 with size 65M
+  go version 1.6.3 matches the manifest provided md5 checksum of 5f7bf9d61d2b0dd75c9e2cd7a87272cc
 
 Downloading godep version v74 from: https://pivotal-buildpacks.s3.amazonaws.com/concourse-binaries/godep/godep-v74-linux-x64.tgz
   Using godep version v74 with size 2.8M
@@ -48,7 +44,7 @@ Downloading godep version v74 from: https://pivotal-buildpacks.s3.amazonaws.com/
     it 'outputs the type of buildpack created, where and its human readable size' do
       expect(subject).to include("Cached buildpack created and saved as")
       expect(subject).to include("spec/fixtures/buildpack/go_buildpack-cached-v1.7.8.zip")
-      expect(subject).to match(/with a size of 164M$/)
+      expect(subject).to match(/with a size of 68M$/)
     end
 
     context 'with a buildpack packager dependency cache intact' do
@@ -56,11 +52,8 @@ Downloading godep version v74 from: https://pivotal-buildpacks.s3.amazonaws.com/
 
       it 'outputs the dependencies downloaded, their versions, and cache location' do
         expect(subject).to include(<<-HEREDOC)
-Using go version 1.6.1 from local cache at: #{tmpdir}/.buildpack-packager/cache/https___storage.googleapis.com_golang_go1.6.1.linux-amd64.tar.gz with size 81M
-  go version 1.6.1 matches the manifest provided md5 checksum of e277d18dac8a0b9945d4c53adf6e52ee
-
-Using go version 1.6.2 from local cache at: #{tmpdir}/.buildpack-packager/cache/https___storage.googleapis.com_golang_go1.6.2.linux-amd64.tar.gz with size 81M
-  go version 1.6.2 matches the manifest provided md5 checksum of ebfb8b38330c8779b121c43433c4b9be
+Using go version 1.6.3 from local cache at: #{tmpdir}/.buildpack-packager/cache/https___buildpacks.cloudfoundry.org_concourse-binaries_go_go1.6.3.linux-amd64.tar.gz with size 65M
+  go version 1.6.3 matches the manifest provided md5 checksum of 5f7bf9d61d2b0dd75c9e2cd7a87272cc
 
 Using godep version v74 from local cache at: #{tmpdir}/.buildpack-packager/cache/https___pivotal-buildpacks.s3.amazonaws.com_concourse-binaries_godep_godep-v74-linux-x64.tgz with size 2.8M
   godep version v74 matches the manifest provided md5 checksum of 70220eee9f9e654e0b85887f696b6add
